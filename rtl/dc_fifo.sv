4662fd39c5f1f366355d859d6132d009b082946f
Joel Vandergriendt <joel.vandergriendt@atomicrules.com>
2024-04-04 20:37:32 -0700

    add better clock callbacks, add dc_fifo_test.cpp

--- rtl/dc_fifo.sv
+++ rtl/dc_fifo.sv
@@ -2,11 +2,128 @@
 /**/
 `timescale 1ns/1ns
 module dc_fifo
-#(parameter type T)
+#(parameter type T=logic [15:0],
+  parameter L2DEPTH=3)
 (
- input logic clka;
- input logic clkb;
- input
+ input wire wr_clk,
+ input wire wr_rstn,
+ input T wr_din,
+ input wire wr_write,
+ output logic wr_full,
+
+ input wire rd_clk,
+ input wire rd_rstn,
+ input wire rd_read,
+ output T rd_dout,
+ output wire rd_empty
 );
-endmodule
+
+
+  reg [$bits(T)-1:0] ram[2**L2DEPTH-1:0];
+
+  logic [L2DEPTH-1:0] w_rptr,w_wptr;
+  logic [L2DEPTH-1:0] r_rptr,r_wptr;
+  logic [L2DEPTH-1:0] r_rptr_plus1,w_wptr_plus1,w_wptr_plus2;
+  cc_gray#(.WIDTH(L2DEPTH) )
+  cc_wptr(.clk_in(wr_clk),
+          .clk_out(rd_clk),
+          .in(w_wptr),
+          .out(r_wptr)),
+  cc_rptr(.clk_in(rd_clk),
+          .clk_out(wr_clk),
+          .in(r_rptr),
+          .out(w_rptr));
+
+  logic wr_prot;
+  assign wr_prot = wr_write && !wr_full;
+  always_ff @(posedge wr_clk)begin
+    wr_full <= w_wptr_plus1 == w_rptr;
+    if(wr_prot)begin
+      ram[w_wptr] <= wr_din;
+      w_wptr_plus2 <= w_wptr_plus2+1;
+      w_wptr_plus1 <= w_wptr_plus2;
+      w_wptr <= w_wptr_plus1;
+      if(w_wptr_plus2 == w_rptr)begin
+        wr_full <= 1'b1;
+      end
+    end
+
+    if(!wr_rstn) begin
+      w_wptr <= '0;
+      w_wptr_plus1 <= 'd1;
+      w_wptr_plus2 <= 'd2;
+    end
+
+  end
+
+
+  logic rd_prot;
+  assign rd_prot = rd_read && !rd_empty;
+  always_ff @(posedge rd_clk)begin
+    rd_dout <= ram[r_rptr];
+    rd_empty <= r_rptr == r_wptr;
+
+    if(rd_prot)begin
+      if (r_rptr_plus1 == r_wptr)begin
+        rd_empty <= 1'b1;
+      end
+      rd_dout <= ram[r_rptr_plus1];
+      r_rptr <= r_rptr_plus1;
+      r_rptr_plus1 <= r_rptr_plus1+1;
+    end
+
+    if(!rd_rstn) begin
+      r_rptr <= '0;
+      r_rptr_plus1 <= 'd1;
+    end
+  end
+
+  `ifdef VERILATOR
+    logic [L2DEPTH-1:0] sim_used;
+  assign sim_used = w_wptr-r_rptr;
+
+
+  `endif
+
+
+
+endmodule // dc_fifo
+
+module cc_gray #(parameter WIDTH)
+  (
+   input wire clk_in,
+   input wire [WIDTH-1:0] in,
+   input wire clk_out,
+   output logic [WIDTH-1:0] out);
+
+  function automatic logic [WIDTH-1:0] gray2bin(logic [WIDTH-1:0] v);
+    gray2bin[WIDTH-1] =v[WIDTH-1];
+    for(int b=WIDTH-2;b>=0;b--)begin
+      gray2bin[b] = v[b]^gray2bin[b+1];
+    end
+  endfunction // grey2bin
+
+  function automatic logic [WIDTH-1:0] bin2gray(logic [WIDTH-1:0] v);
+    bin2gray[WIDTH-1] = v[WIDTH-1];
+    for(int b=WIDTH-2;b>=0;b--)begin
+      bin2gray[b] = v[b]^v[b+1];
+    end
+  endfunction
+
+
+  logic [WIDTH-1:0] ff_gray;
+  logic [WIDTH-1:0] ff_gray_out0;
+  logic [WIDTH-1:0] ff_gray_out1;
+  logic [WIDTH-1:0] ff_bin_out;
+  always_ff @(posedge(clk_in) ) begin
+    ff_gray <= bin2gray(in);
+  end;
+  always_ff @(posedge(clk_out) ) begin
+    ff_gray_out0 <= ff_gray;
+    ff_gray_out1 <= ff_gray_out0;
+    ff_bin_out <= gray2bin(ff_gray_out1);
+  end;
+  assign out = ff_bin_out;
+
+  endmodule
 `default_nettype wire
--- verilator/csrc/verilator_driver.hpp
+++ verilator/csrc/verilator_driver.hpp
@@ -4,6 +4,7 @@
 #include <chrono>
 #include <memory>
 #include <ratio>
+#include <stdexcept>
 #include <vector>
 #include "verilated.h"
 #include <verilated_fst_c.h>
@@ -18,18 +19,20 @@
 	}				   \
     }while(0)
 
+
 class ClockDriver{
 public:
     using duration_t = std::chrono::nanoseconds;
     enum class edge_e { RISE_EDGE,FALL_EDGE,BOTH_EDGE};
 private:
     duration_t  down_time,up_time;
-    duration_t last_update;
+    duration_t m_last_update;
      ///< Function to set the value of the clock net
     struct callback_t {
-	std::function<void(uint8_t)> fun;
+	std::function<void(edge_e)> fun;
 	edge_e edge;
     };
+    std::function<void(uint8_t)> clock_fun;
     std::vector<callback_t> callback_functions;
     int clk_val;
 public:
@@ -37,39 +40,47 @@ public:
         {
 	    up_time = period/2;
 	    down_time = period -up_time;
-	    callback_functions.push_back({fun,edge_e::BOTH_EDGE});
+	    clock_fun = fun;
 	}
-    void add_callback(std::function < void(uint8_t)> fun, edge_e e=edge_e::RISE_EDGE) {
+    void add_callback(std::function < void(edge_e)> fun, edge_e e=edge_e::RISE_EDGE) {
+
 	callback_functions.push_back({fun,e});
     }
     duration_t next_update(){
 	if(clk_val){
-	    return last_update+up_time;
+	    return m_last_update+up_time;
 	}else{
-	    return last_update+down_time;
+	    return m_last_update+down_time;
 	}
     }
     duration_t get_period(){
         return down_time+up_time;
     }
-
+    duration_t last_update(){
+	return m_last_update;
+    }
     void update(duration_t now){
 	clk_val=!clk_val;
+	clock_fun(clk_val);
+        m_last_update = now;
+    }
+    void exec_callbacks(){
 	edge_e ignore = clk_val ? edge_e::FALL_EDGE : edge_e::RISE_EDGE;
+	edge_e edge = clk_val ? edge_e::RISE_EDGE : edge_e::FALL_EDGE;
 	for(auto &cb : callback_functions){
 	    if (cb.edge != ignore){
-		cb.fun(clk_val);
+		cb.fun(edge);
 	    }
 	}
-        last_update = now;
     }
 };
 template <typename dut_t> class verilator_driver {
     using duration_t = ClockDriver::duration_t;
-    std::vector<ClockDriver> m_clocks;
+    std::vector<std::reference_wrapper<ClockDriver>> m_clocks;
 
     VerilatedContext* m_context;
     VerilatedFstC*  m_trace;
+    duration_t sim_timeout;
 protected:
     dut_t*  dut;
     verilator_driver(int argc,char** argv)
@@ -83,33 +94,44 @@ protected:
 
 	    dut->trace(m_trace,99);
 	    m_trace->open("test.fst");
+	    sim_timeout=std::chrono::milliseconds(10);
     }
     ~verilator_driver(){
 	delete m_trace;
 	delete dut;
 	delete m_context;
     }
-    void add_clock(ClockDriver cd){
+    void add_clock(ClockDriver& cd){
 	m_clocks.push_back(cd);
     }
     duration_t update(){
-        duration_t min_update = m_clocks.begin()->next_update();
+        duration_t min_update = m_clocks.begin()->get().next_update();
 	duration_t now=duration_t(m_context->time());
-        for(auto &c: m_clocks){
+        for(auto &cd: m_clocks){
+	    auto &c  = cd.get();
             if(c.next_update() < min_update)
                 min_update= c.next_update();
         }
 
         dut->eval();
 	m_trace->dump(m_context->time());
-	for(auto &c: m_clocks){
+	for(auto &cd: m_clocks){
+	    auto &c  = cd.get();
 	    if(c.next_update() == min_update){
 		c.update(min_update);
 	    }
         }
 	m_context->time(min_update.count());
 	dut->eval();
-
+        for (auto &cd : m_clocks) {
+	    auto &c  = cd.get();
+	    if(c.last_update() == min_update){
+		c.exec_callbacks();
+	    }
+	}
+	if(sim_timeout != std::chrono::milliseconds(0) && now >= sim_timeout){
+	    throw std::runtime_error("Simulation timed out\n");
+	}
 	return min_update-now;
 
 
