d9a15829917d2890fcc48e1723018319b2a221e6
Joel Vandergriendt <joelvandergriendt@microridge.ca>
2025-01-17 16:12:01 -0800

    counter testbench compiles and runs with vivado

--- xsim/testbench.cpp
+++ xsim/testbench.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 
 #include "xsi_loader.h"
-
+#include "xsi.h"
 const char* expected_out[15] = {"0001", "0010", "0011", "0100", "0101", "0110", "0111", "1000", "1001", "1010", "1011", "1100", "1101", "1110", "1111"};
 
 void append_logic_val_bit_to_string(std::string& retVal, int aVal, int bVal)
@@ -118,58 +118,58 @@ int main(int argc, char **argv)
         info.logFileName = NULL;
         char wdbName[] = "test.wdb";
         info.wdbFileName = wdbName;
-        Xsi_Instance.open(&info);
-        Xsi_Instance.trace_all();
-        reset = Xsi_Instance.get_port_number("reset");
+        xsiHandle handle=xsi_open(&info);
+        xsi_trace_all(handle);
+        reset = xsi_get_port_number(handle,"reset");
         if(reset <0) {
           std::cerr << "ERROR: reset not found" << std::endl;
           exit(1);
         }
-        clk = Xsi_Instance.get_port_number("clk");
+        clk = xsi_get_port_number(handle,"clk");
         if(clk <0) {
           std::cerr << "ERROR: clk not found" << std::endl;
           exit(1);
         }
-        enable = Xsi_Instance.get_port_number("enable");
+        enable = xsi_get_port_number(handle,"enable");
         if(enable <0) {
           std::cerr << "ERROR: enable not found" << std::endl;
           exit(1);
         }
-        count = Xsi_Instance.get_port_number("count");
+        count = xsi_get_port_number(handle,"count");
         if(count <0) {
           std::cerr << "ERROR: count not found" << std::endl;
           exit(1);
         }
 
         // Start low clock
-        Xsi_Instance.put_value(clk, &zero_val);
-        Xsi_Instance.run(10);
+        xsi_put_value(handle,clk, (void*)&zero_val);
+        xsi_run(handle,10);
 
         // Reset to 1 and clock it
-        Xsi_Instance.put_value(reset, &one_val);
-        Xsi_Instance.put_value(enable, &zero_val);
-        Xsi_Instance.put_value(clk, &one_val);
-        Xsi_Instance.run(10);
+        xsi_put_value(handle,reset, (void*)&one_val);
+        xsi_put_value(handle,enable, (void*)&zero_val);
+        xsi_put_value(handle,clk, (void*)&one_val);
+        xsi_run(handle,10);
 
         // Put clk to 0, reset to 0 and enable to 1
-        Xsi_Instance.put_value(clk, &zero_val);
-        Xsi_Instance.put_value(reset, &zero_val);
-        Xsi_Instance.put_value(enable, &one_val);
-        Xsi_Instance.run(10);
+        xsi_put_value(handle,clk, (void*)&zero_val);
+        xsi_put_value(handle,reset, (void*)&zero_val);
+        xsi_put_value(handle,enable, (void*)&one_val);
+        xsi_run(handle,10);
 
         //Test get_time functionality
-        int64_t sim_time = Xsi_Instance.get_time();
+        int64_t sim_time = xsi_get_time(handle);
         std::cout << "Simulation time: " << sim_time << std::endl;
 
         std::string count_val_string;
         // The reset is done. Now start counting
         std::cout << "\n *** starting to count ***\n";
         for (int i=0; i < 15; i++) {
-           Xsi_Instance.put_value(clk, &one_val);
-           Xsi_Instance.run(10);
+           xsi_put_value(handle,clk, (void*)&one_val);
+           xsi_run(handle,10);
 
            // read the output
-           Xsi_Instance.get_value(count, &count_val);
+           xsi_get_value(handle,count, &count_val);
 
            count_val_string = logic_val_to_string(&count_val, 4);
            std::cout << count_val_string << std::endl;
@@ -177,10 +177,10 @@ int main(int argc, char **argv)
               count_success++;
            }
            // Put clk to zero
-           Xsi_Instance.put_value(clk, &zero_val);
-           Xsi_Instance.run(10);
+           xsi_put_value(handle,clk, (void*)&zero_val);
+           xsi_run(handle,10);
         }
-        std::string errorInfo = Xsi_Instance.get_error_info();
+        std::string errorInfo = xsi_get_error_info(handle);
         std::cout << "ERROR info " << errorInfo << std::endl;
 
         std::cout << "\n *** done counting ***\n";
@@ -189,7 +189,7 @@ int main(int argc, char **argv)
         status = (count_success == 15) ? 0:1;
 
         // Just a check to rewind time to 0
-        Xsi_Instance.restart();
+        xsi_restart(handle);
 
     }
     catch (std::exception& e) {
